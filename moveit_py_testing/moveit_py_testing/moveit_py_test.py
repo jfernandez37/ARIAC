271926cbcd5c80141ccc05c54b9c0b61c3983d9d
Joseph Fernandez <jothomas0615@gmail.com>
2023-11-16 14:37:32 -0500

    Can now move to a cartesian pose

--- moveit_py_testing/moveit_py_testing/moveit_py_test.py
+++ moveit_py_testing/moveit_py_testing/moveit_py_test.py
@@ -204,6 +204,21 @@ def main(args=None):
     ceiling_robot.set_goal_state(configuration_name = "home")
     plan_and_execute(robot, ceiling_robot, logger, sleep_time=0.0)
 
+    robot_model = robot.get_robot_model()
+    planning_scene_monitor = robot.get_planning_scene_monitor()
+    for i in range(1,len(bin_positions)+1):
+        bin_pose = Pose()
+        bin_pose.position.x = float(bin_positions_dict[f"bin{i}"][0])
+        bin_pose.position.y = float(bin_positions_dict[f"bin{i}"][1])
+        bin_pose.position.z = 0.0
+        # quaternion
+        bin_pose.orientation.x= 0.0
+        bin_pose.orientation.y = 0.0
+        bin_pose.orientation.z = 0.0
+        bin_pose.orientation.w = 1.0
+        add_model_to_planning_scene(f"bin{i}","bin.stl",logger, planning_scene_monitor, bin_pose)
+    logger.info("Finished adding bins to planning scene")
+
     # floor_robot.set_start_state_to_current_state()
     # floor_robot.set_goal_state(configuration_name="extended")
     # plan_and_execute(robot, floor_robot, logger, sleep_time=3.0)
@@ -232,188 +247,167 @@ def main(args=None):
     # # -------------------------------------------------------
     # #                         Test 3
     # # -------------------------------------------------------
+    with planning_scene_monitor.read_write() as scene:
+        robot_state = scene.current_state
+        robot_state.update()
+        # set plan start state to current state
+        floor_robot.set_start_state_to_current_state()
 
-    # # set plan start state to current state
-    # floor_robot.set_start_state_to_current_state()
+        # set pose goal with PoseStamped message
+        pose_goal = PoseStamped()
+        pose_goal.header.frame_id = "floor_base_link_inertia"
+        pose_goal.pose.orientation.w = 1.0
+        pose_goal.pose.position.x = 0.9
+        pose_goal.pose.position.y = -0.3
+        pose_goal.pose.position.z = 0.5
+        floor_robot.set_goal_state(pose_stamped_msg=pose_goal, pose_link="floor_gripper")
 
-    # # set pose goal with PoseStamped message
-    # pose_goal = PoseStamped()
-    # pose_goal.header.frame_id = "floor_base_link_inertia"
-    # pose_goal.pose.orientation.w = 1.0
-    # pose_goal.pose.position.x = 0.5
-    # pose_goal.pose.position.y = -0.2
-    # pose_goal.pose.position.z = 0.25
-    # floor_robot.set_goal_state(pose_stamped_msg=pose_goal, pose_link="floor_gripper")
+        # plan to goal
+    plan_and_execute(robot, floor_robot, logger)
 
-    # # plan to goal
-    # plan_and_execute(robot, floor_robot, logger)
+    # with planning_scene_monitor.read_write() as scene:
 
-    # while True:
-    #     try:
-    #         (int)
-    #     except KeyboardInterrupt:
-    #         break
-    # robot_model = robot.get_robot_model()
-    # robot_state = RobotState(robot_model)
+    #     # instantiate a RobotState instance using the current robot model
+    #     robot_state = scene.current_state
+    #     robot_state.update()
+    #     # header
+    #     sleep(1.0)
+    #     header = Header()
+    #     header.frame_id = "world"
+    #     header.stamp = node.get_clock().now().to_msg()
+
+    #     # link model names
+    #     link_model_names = robot.get_robot_model().get_joint_model_group("floor_robot").link_model_names
+    #     link_model_names = ["floor_gripper"]
+
+    #     # Robot State
+    #     sleep(1.0)
+        
+        
+    #     robot_state_msg = robotStateToRobotStateMsg(robot_state)
+    #     robot_state.update()
+
+    #     fk_posestamped = node.call_get_position_fk(header, link_model_names, robot_state_msg)
+
+    #     # header
+    #     sleep(1.0)
+    #     header = Header()
+    #     header.frame_id = "world"
+    #     header.stamp = node.get_clock().now().to_msg()
+
+    #     # Group name
+    #     group_name = "floor_robot"
+
+    #     # Link name
+    #     link_name = "floor_gripper"
+
+    #     # Waypoints
+    #     logger.info(str(len(fk_posestamped)))
+    #     current_pose = fk_posestamped[-1].pose
+    #     logger.info("Current position: " + str(current_pose.position))
+    #     desired_pose = copy(current_pose)
+    #     logger.info("before: "+str(desired_pose))
+    #     desired_pose.position.x-=0.1
+    #     desired_pose.position.y-=3
+    #     desired_pose.position.z-=0.1
+    #     logger.info("after: "+str(desired_pose))
+    #     desired_pose_2 = copy(desired_pose)
+    #     desired_pose.position.x-=0.3
+    #     desired_pose.position.y+=6
+    #     desired_pose.position.z+=0.25
+    #     waypoints = [desired_pose]
+
+        
+
+    #     # Max step
+    #     max_step = 0.1
+    #     robot_state.update()
+    #     robot_state_msg = robotStateToRobotStateMsg(robot_state)
+    #     trajectory_msg = node.call_get_cartesian_path(header,
+    #                                 robot_state_msg,
+    #                                 group_name,
+    #                                 link_name,
+    #                                 waypoints,
+    #                                 max_step)
+    #     robot_state.update()
+    #     trajectory = RobotTrajectory(robot_model)
+    #     trajectory.set_robot_trajectory_msg(robot_state, trajectory_msg)
+    #     trajectory.joint_model_group_name = "floor_robot"
     # robot_state.update(True)
-    # logger.info("\n".join(str(robotStateToRobotStateMsg(robot_state)).split(",")))
-    robot_model = robot.get_robot_model()
-    planning_scene_monitor = robot.get_planning_scene_monitor()
-    for i in range(1,len(bin_positions)+1):
-        bin_pose = Pose()
-        bin_pose.position.x = float(bin_positions_dict[f"bin{i}"][0])
-        bin_pose.position.y = float(bin_positions_dict[f"bin{i}"][1])
-        bin_pose.position.z = 0.0
-        # quaternion
-        bin_pose.orientation.x= 0.0
-        bin_pose.orientation.y = 0.0
-        bin_pose.orientation.z = 0.0
-        bin_pose.orientation.w = 1.0
-        add_model_to_planning_scene(f"bin{i}","bin.stl",logger, planning_scene_monitor, bin_pose)
+    # robot.execute(trajectory, controllers=[])
 
-    with planning_scene_monitor.read_write() as scene:
+    # with planning_scene_monitor.read_write() as scene:
 
-        # instantiate a RobotState instance using the current robot model
-        robot_state = scene.current_state
-        robot_state.update()
-        # header
-        sleep(1.0)
-        header = Header()
-        header.frame_id = "world"
-        header.stamp = node.get_clock().now().to_msg()
+    #     # instantiate a RobotState instance using the current robot model
+    #     robot_state = scene.current_state
+    #     robot_state.update()
+    #     # header
+    #     sleep(1.0)
+    #     header = Header()
+    #     header.frame_id = "world"
+    #     header.stamp = node.get_clock().now().to_msg()
 
-        # link model names
-        link_model_names = robot.get_robot_model().get_joint_model_group("floor_robot").link_model_names
-        link_model_names = ["floor_gripper"]
+    #     # link model names
+    #     link_model_names = robot.get_robot_model().get_joint_model_group("floor_robot").link_model_names
+    #     link_model_names = ["floor_gripper"]
 
-        # Robot State
-        sleep(1.0)
+    #     # Robot State
+    #     sleep(1.0)
         
         
-        robot_state_msg = robotStateToRobotStateMsg(robot_state)
-        robot_state.update()
+    #     robot_state_msg = robotStateToRobotStateMsg(robot_state)
+    #     robot_state.update()
+    #     logger.info("\n".join(str(robotStateToRobotStateMsg(robot_state)).split(",")))
 
-        fk_posestamped = node.call_get_position_fk(header, link_model_names, robot_state_msg)
+    #     fk_posestamped = node.call_get_position_fk(header, link_model_names, robot_state_msg)
 
-        # header
-        sleep(1.0)
-        header = Header()
-        header.frame_id = "world"
-        header.stamp = node.get_clock().now().to_msg()
+    #     # header
+    #     sleep(1.0)
+    #     header = Header()
+    #     header.frame_id = "world"
+    #     header.stamp = node.get_clock().now().to_msg()
 
-        # Group name
-        group_name = "floor_robot"
+    #     # Group name
+    #     group_name = "floor_robot"
 
-        # Link name
-        link_name = "floor_gripper"
+    #     # Link name
+    #     link_name = "floor_gripper"
 
-        # Waypoints
-        logger.info(str(len(fk_posestamped)))
-        current_pose = fk_posestamped[-1].pose
-        logger.info("Current position: " + str(current_pose.position))
-        desired_pose = copy(current_pose)
-        logger.info("before: "+str(desired_pose))
-        desired_pose.position.x-=0.1
-        desired_pose.position.y-=3
-        desired_pose.position.z-=0.1
-        logger.info("after: "+str(desired_pose))
-        desired_pose_2 = copy(desired_pose)
-        desired_pose.position.x-=0.3
-        desired_pose.position.y+=6
-        desired_pose.position.z+=0.25
-        waypoints = [desired_pose]
+    #     # Waypoints
+    #     logger.info(str(len(fk_posestamped)))
+    #     current_pose = fk_posestamped[-1].pose
+    #     logger.info("Current position: " + str(current_pose.position))
+    #     desired_pose = copy(current_pose)
+    #     logger.info("before: "+str(desired_pose))
+    #     # desired_pose.position.x-=0.1
+    #     desired_pose.position.y-=0.5
+    #     # desired_pose.position.z-=0.1
+    #     logger.info("after: "+str(desired_pose))
+    #     waypoints = [desired_pose]
 
         
 
-        # Max step
-        max_step = 0.1
-        robot_state.update()
-        robot_state_msg = robotStateToRobotStateMsg(robot_state)
-        trajectory_msg = node.call_get_cartesian_path(header,
-                                    robot_state_msg,
-                                    group_name,
-                                    link_name,
-                                    waypoints,
-                                    max_step)
-        robot_state.update()
-        trajectory = RobotTrajectory(robot_model)
-        trajectory.set_robot_trajectory_msg(robot_state, trajectory_msg)
-        trajectory.joint_model_group_name = "floor_robot"
-    robot_state.update(True)
-    robot.execute(trajectory, controllers=[])
-
-    with planning_scene_monitor.read_write() as scene:
-
-        # instantiate a RobotState instance using the current robot model
-        robot_state = scene.current_state
-        robot_state.update()
-        # header
-        sleep(1.0)
-        header = Header()
-        header.frame_id = "world"
-        header.stamp = node.get_clock().now().to_msg()
-
-        # link model names
-        link_model_names = robot.get_robot_model().get_joint_model_group("floor_robot").link_model_names
-        link_model_names = ["floor_gripper"]
-
-        # Robot State
-        sleep(1.0)
-        
-        
-        robot_state_msg = robotStateToRobotStateMsg(robot_state)
-        robot_state.update()
-        logger.info("\n".join(str(robotStateToRobotStateMsg(robot_state)).split(",")))
-
-        fk_posestamped = node.call_get_position_fk(header, link_model_names, robot_state_msg)
-
-        # header
-        sleep(1.0)
-        header = Header()
-        header.frame_id = "world"
-        header.stamp = node.get_clock().now().to_msg()
-
-        # Group name
-        group_name = "floor_robot"
-
-        # Link name
-        link_name = "floor_gripper"
-
-        # Waypoints
-        logger.info(str(len(fk_posestamped)))
-        current_pose = fk_posestamped[-1].pose
-        logger.info("Current position: " + str(current_pose.position))
-        desired_pose = copy(current_pose)
-        logger.info("before: "+str(desired_pose))
-        # desired_pose.position.x-=0.1
-        desired_pose.position.y-=0.5
-        # desired_pose.position.z-=0.1
-        logger.info("after: "+str(desired_pose))
-        waypoints = [desired_pose]
-
-        
-
-        # Max step
-        max_step = 0.1
-        robot_state.update()
-        robot_state_msg = robotStateToRobotStateMsg(robot_state)
-        trajectory_msg = node.call_get_cartesian_path(header,
-                                    robot_state_msg,
-                                    group_name,
-                                    link_name,
-                                    waypoints,
-                                    max_step)
-        robot_state.update()
-        trajectory = RobotTrajectory(robot_model)
-        trajectory.set_robot_trajectory_msg(robot_state, trajectory_msg)
-        trajectory.joint_model_group_name = "floor_robot"
-    robot_state.update(True)
-    robot.execute(trajectory, controllers=[])
-    try:
-        while(True):
-            sleep(1)
-    except KeyboardInterrupt:
-        (int)
+    #     # Max step
+    #     max_step = 0.1
+    #     robot_state.update()
+    #     robot_state_msg = robotStateToRobotStateMsg(robot_state)
+    #     trajectory_msg = node.call_get_cartesian_path(header,
+    #                                 robot_state_msg,
+    #                                 group_name,
+    #                                 link_name,
+    #                                 waypoints,
+    #                                 max_step)
+    #     robot_state.update()
+    #     trajectory = RobotTrajectory(robot_model)
+    #     trajectory.set_robot_trajectory_msg(robot_state, trajectory_msg)
+    #     trajectory.joint_model_group_name = "floor_robot"
+    # robot_state.update(True)
+    # robot.execute(trajectory, controllers=[])
+    # try:
+    #     while(True):
+    #         sleep(1)
+    # except KeyboardInterrupt:
+        # (int)
     rclpy.shutdown()
 
 if __name__ == "__main__":
